41e34d77b034253bce225a214245078ddfc5fd34
MarwanDev <marwaneg@aucegypt.edu>
2023-03-15 19:10:58 +0200

    Add professor button functionality

--- App.js
+++ App.js
@@ -16,6 +16,24 @@ const professorsArray = [
     studies: 'Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in 2006',
     imgSrc: 'assets/julia_reda.jpg',
   },
+  {
+    name: 'Yochai Benkler',
+    summary: 'Berkman Professor of EnterPreneurial Legal Studies at Harvard Law School',
+    studies: 'Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in 2006',
+    imgSrc: 'assets/julia_reda.jpg',
+  },
+  {
+    name: 'Yochai Benkler',
+    summary: 'Berkman Professor of EnterPreneurial Legal Studies at Harvard Law School',
+    studies: 'Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in 2006',
+    imgSrc: 'assets/julia_reda.jpg',
+  },
+  {
+    name: 'Yochai Benkler',
+    summary: 'Berkman Professor of EnterPreneurial Legal Studies at Harvard Law School',
+    studies: 'Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in 2006',
+    imgSrc: 'assets/julia_reda.jpg',
+  },
 ];
 
 const professorsContainer = document.getElementById('professors-list');
@@ -31,14 +49,36 @@ const professorsDisplay = (name, summary, studies, imgSrc) => `<div class="profe
   </div>
 </div>`;
 
-professorsArray.forEach((professor) => {
-  const htmlToAdd = professorsDisplay(professor.name,
-    professor.summary,
-    professor.studies,
-    professor.imgSrc);
-  professorsContainer.insertAdjacentHTML('afterbegin', htmlToAdd);
-});
-
+const professorsButton = document.getElementById('more-professors-btn');
+if (professorsButton.style.display === 'none') {
+  for (let i = 0; i < professorsArray.length; i += 1) {
+    const htmlToAdd = professorsDisplay(professorsArray[i].name,
+      professorsArray[i].summary,
+      professorsArray[i].studies,
+      professorsArray[i].imgSrc);
+    professorsContainer.insertAdjacentHTML('afterbegin', htmlToAdd);
+  }
+} else {
+  for (let i = 0; i < 2; i += 1) {
+    const htmlToAdd = professorsDisplay(professorsArray[i].name,
+      professorsArray[i].summary,
+      professorsArray[i].studies,
+      professorsArray[i].imgSrc);
+    professorsContainer.insertAdjacentHTML('afterbegin', htmlToAdd);
+  }
+  professorsButton.addEventListener('click', () => {
+    for (let i = 2; i < professorsArray.length; i += 1) {
+      const htmlToAdd = professorsDisplay(professorsArray[i].name,
+        professorsArray[i].summary,
+        professorsArray[i].studies,
+        professorsArray[i].imgSrc);
+      professorsContainer.insertAdjacentHTML('afterbegin', htmlToAdd);
+    }
+    professorsButton.style.display = 'none';
+  });
+}
+// });
+// console.log(professorsArray[1].imgSrc);
 const date = new Date();
 
 const day = date.getDate();
