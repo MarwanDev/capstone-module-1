7f64e95a95ae514c2de3ab511f425254717cd6b7
MarwanDev <marwaneg@aucegypt.edu>
2023-03-16 09:00:30 +0200

    Update resources

--- App.js
+++ App.js
@@ -5,34 +5,40 @@ function toggleMobileMenu(menu) {
 
 const professorsArray = [
   {
-    name: 'Matt Leaman',
-    summary: 'Berkman Professor of EnterPreneurial Legal Studies at Harvard Law School',
-    studies: 'Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in 2006',
-    imgSrc: 'assets/mattl.jpg',
+    name: 'SohYeong Noh',
+    summary: 'Director of Art Centre Nabi and a board member of CC Korea',
+    studies: 'As the main venue for new media art production in Korea, Nabi promotes cross-disciplinary collaboration and understanding among science technology, humanities, and the arts.',
+    imgSrc: 'assets/soh-yeong-roh_blog.jpg',
   },
   {
     name: 'Yochai Benkler',
     summary: 'Berkman Professor of EnterPreneurial Legal Studies at Harvard Law School',
     studies: 'Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in 2006',
-    imgSrc: 'assets/julia_reda.jpg',
+    imgSrc: 'assets/yochai_benkler.jpg',
   },
   {
-    name: 'ryan merkley',
+    name: 'Ryan Merkley',
     summary: 'Berkman Professor of EnterPreneurial Legal Studies at Harvard Law School',
     studies: 'Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in 2006',
     imgSrc: 'assets/ryanmerkley.jpg',
   },
   {
     name: 'Lila Tretikov',
-    summary: 'Berkman Professor of EnterPreneurial Legal Studies at Harvard Law School',
-    studies: 'Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in 2006',
+    summary: 'Executive Director of the Wikimedia Foundation',
+    studies: 'Lila Tretikov is the Executive of the Wikimedia Foundation, the nonprofit organization that operates Wikipedia. Wikipedia is freely available in 290 languag-es and used by nearly half a billion people around the world every month.',
     imgSrc: 'assets/Lila_Tretikov_600.jpg',
   },
   {
     name: 'Kilnam Chon',
     summary: 'Berkman Professor of EnterPreneurial Legal Studies at Harvard Law School',
-    studies: 'Benkler studies commons-based peer production, and published his seminal book The Wealth of Networks in 2006',
+    studies: 'Kilnam Chon helped bring the internet to Asia and is an outspoken advocate for the open web and digital com-mons. In 2012. he was inducted into the inaugural class of the Internet Society’s (ISOC) Internet Hall of Fame',
     imgSrc: 'assets/KilnamChonPortrait.jpg',
+  },
+  {
+    name: 'Julia Leda',
+    summary: 'President of Young Pirates of Europe',
+    studies: 'European ingetration, political democracy and participation of youth through online as her major condern, Reda’s report outlining potential changes to EU copyright law was approved by the Parliament in July',
+    imgSrc: 'assets/julia_reda.jpg',
   },
 ];
 
@@ -59,7 +65,7 @@ if (professorsButton.style.display === 'none') {
     professorsContainer.insertAdjacentHTML('afterbegin', htmlToAdd);
   }
 } else {
-  for (let i = professorsArray.length - 2; i < professorsArray.length; i += 1) {
+  for (let i = professorsArray.length - 3; i < professorsArray.length; i += 1) {
     const htmlToAdd = professorsDisplay(professorsArray[i].name,
       professorsArray[i].summary,
       professorsArray[i].studies,
